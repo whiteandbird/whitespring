b9efd31935c1ae31641b38568c70f8ef06b313fd
wangduanyue <wangduanyue@huice.com>
2021-10-14 16:53:03 +0800

    Bean工厂以及Bean定义

---
+++ src/test/java/com/wang/User.java
@@ -0,0 +1,11 @@
+package com.wang;
+
+/**
+ * @author wangdy
+ * @date 2021/10/14 16:47
+ */
+public class User {
+    public void say(){
+        System.out.println("hello world, this is my springframework");
+    }
+}
