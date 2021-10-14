200f0ef80f67a790c945e6807dfd173c515b003c
wangduanyue <wangduanyue@huice.com>
2021-10-14 11:10:20 +0800

    简易版spring初始化工作和环境测试

---
+++ src/main/java/com/wang/test/MainTest.java
@@ -0,0 +1,11 @@
+package com.wang.test;
+
+/**
+ * @author wangdy
+ * @date 2021/10/14 11:09
+ */
+public class MainTest {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
