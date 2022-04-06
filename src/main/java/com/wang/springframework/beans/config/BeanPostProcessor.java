7d4925d6062c864f76627d5562fbcd9da4d65773
wangduanyue <wangduanyue@huice.com>
2022-04-06 19:20:32 +0800

    完善beanPostProcessor接口

--- src/main/java/com/wang/springframework/beans/config/BeanPostProcessor.java
+++ src/main/java/com/wang/springframework/beans/config/BeanPostProcessor.java
@@ -5,4 +5,7 @@ package com.wang.springframework.beans.config;
  * @date 2022/3/31 20:06
  */
 public interface BeanPostProcessor {
+    Object postProcessorBeforeInitialization(Object bean,String beanName);
+
+    Object postProcessorAfterInitialization(Object bean,String beanName);
 }
