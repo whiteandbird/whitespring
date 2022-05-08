1ed660d5495a8d63424cdbd5ef9689693e4c1509
whiteandbird <1096576877@qq.com>
2022-05-08 20:01:21 +0800

    注释

--- src/main/java/com/wang/springframework/beans/factory/support/AbstractBeanFactory.java
+++ src/main/java/com/wang/springframework/beans/factory/support/AbstractBeanFactory.java
@@ -48,13 +48,23 @@ public abstract class AbstractBeanFactory extends FactoryBeanRegisterSupport imp
         return doGetBean(beanName, args);
     }
 
+    /**
+     * 该方法通过判断bean的类型来决定是否返回普通的或者factoryBean里面的数据
+     * @param beanName
+     * @param args
+     * @param <T>
+     * @return
+     */
     protected <T> T doGetBean(String beanName,Object[] args){
         Object shareInstance = getSingleton(beanName);
+        // 拿到bean 但是首先得给factoryBean一个机会  让其有机会
         if(shareInstance != null){
+            // 给factoryBean一个机会
             return (T) getObjectForBeanInstance(shareInstance, beanName);
         }
         // 单例里面没有 那么就要调用创建方法
         shareInstance = createBean(beanName, getBeanDefinition(beanName), args);
+        // 创建完了还是得给factoryBean一个机会
         return (T) getObjectForBeanInstance(shareInstance, beanName);
     }
 
@@ -62,8 +72,10 @@ public abstract class AbstractBeanFactory extends FactoryBeanRegisterSupport imp
         if(! (bean instanceof FactoryBean)){
             return bean;
         }
+        // 判断为factoryBean 那么需要执行获取factoryBean对象 先从factoryBean缓存里面拿
         Object factoryBean = getCachedForFactoryBean(beanName);
         if(null == factoryBean){
+            // 缓存里面没有 那么就得去生成
             FactoryBean factoryBeanInstance = (FactoryBean) bean;
             factoryBean = getObjectFromFactoryBean(factoryBeanInstance, beanName);
         }
--- src/main/java/com/wang/test/FactoryBeanTest.java
+++ src/main/java/com/wang/test/FactoryBeanTest.java
@@ -1,6 +1,7 @@
 package com.wang.test;
 
 import com.wang.springframework.context.support.ClassPathXmlApplicationContext;
+import com.wang.test.dao.IUserDao;
 import com.wang.test.dao.UserService;
 
 /**
@@ -12,8 +13,7 @@ public class FactoryBeanTest {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath:factoryBeans.xml");
         context.registerShutHook();
-        UserService bean = context.getBean(UserService.class);
-        String query = bean.query("1");
-        System.out.println("quer 结果"+query);
+        IUserDao bean = (IUserDao)context.getBean(MyFactoryBean.class);
+        System.out.println("quer 结果"+bean.getQuery("wang"));
     }
 }
